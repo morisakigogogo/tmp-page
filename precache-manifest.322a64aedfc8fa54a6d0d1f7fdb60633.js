33f29096db92490f9e19ea606ae3bf96b14e5508
kan.gakukai <hirose@morisakihiroshishounoMacBook-Pro.local>
2020-01-21 16:50:24 +0900

    first commit

---
+++ precache-manifest.322a64aedfc8fa54a6d0d1f7fdb60633.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f878485b47ad36b8a9c3b50d8ba40d5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89436721ca92cf8e72a7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7c9841be1edd414b2756",
+    "url": "/static/js/2.886d49af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.886d49af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89436721ca92cf8e72a7",
+    "url": "/static/js/main.f116fa01.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
